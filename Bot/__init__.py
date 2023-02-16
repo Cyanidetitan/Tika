64d9a5e1834f2613ad35494f1742de3045ad841f
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-16 23:11:50 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -11,13 +11,13 @@ LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'your bot token') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 456789)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
-MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '5912998821:AAHnfwQMIgzQ5GgaAw1iyiDIts11pn2SKkY') #BOT Token Add
+API_ID = int(os.environ.get('API_ID', 8858279)) #Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', 'ef28c3f458143cbcb4271a98a2e9d596')# Telgram App hash  
+OWNER_ID = int(os.environ.get('OWNER_ID',5371981851 ))
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://auto:auto@cluster0.iqhle1n.mongodb.net/?retryWrites=true&w=majority') #MONGO DB FOR ANIME DATA
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001258776101)
+BOT_NAME = os.environ.get('BOT_NAME', 'Sido')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
